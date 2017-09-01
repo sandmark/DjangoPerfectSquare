2637f5bfef5c8b741793e33922f229b6bf6c0281
sandmark <sandmark.m@gmail.com>
2017-09-02 00:39:25 +0900

    Add `tagged_contents(tag)` to look contents up by tag

--- cms/urls.py
+++ cms/urls.py
@@ -4,4 +4,5 @@ from cms import views
 urlpatterns = [
     url(r'^$', views.index, name='index'),
     url(r'^watch/(?P<content_id>\d+)/$', views.watch, name='watch'),
+    url(r'^tag/(?P<tag_id>\d+)/contents/$', views.tagged_contents, name='tagged_contents'),
 ]
--- cms/views.py
+++ cms/views.py
@@ -3,7 +3,7 @@ from django.contrib.auth.decorators import login_required
 
 from django.shortcuts import get_object_or_404
 
-from .models import Content
+from .models import Content, Tag
 
 # Create your views here.
 @login_required
@@ -20,3 +20,10 @@ def watch(request, content_id):
     content = get_object_or_404(Content, pk=content_id)
     context = {'content': content}
     return render(request, 'cms/watch.html', context)
+
+@login_required
+def tagged_contents(request, tag_id):
+    tag = get_object_or_404(Tag, pk=tag_id)
+    contents = Content.objects.filter(tags__id__exact=tag.id)
+    context = {'contents': contents, 'tag': tag}
+    return render(request, 'cms/tagged_contents.html', context)
