b0037a9fe24fe8296d74d0352420edf8cfab151c
sandmark <sandmark.m@gmail.com>
2017-09-01 20:56:55 +0900

    Add web-player for mp4 files

--- cms/views.py
+++ cms/views.py
@@ -1,6 +1,8 @@
 from django.shortcuts import render
 from django.contrib.auth.decorators import login_required
 
+from django.shortcuts import get_object_or_404
+
 from .models import Content
 
 # Create your views here.
@@ -12,3 +14,9 @@ def index(request):
         'no_contents': not bool(recent_contents)
     }
     return render(request, 'cms/index.html', context)
+
+@login_required
+def watch(request, content_id):
+    content = get_object_or_404(Content, pk=content_id)
+    context = {'content': content}
+    return render(request, 'cms/watch.html', context)
