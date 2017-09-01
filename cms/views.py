c31f743b77c23d0e214a04776357c3b8d409f643
sandmark <sandmark.m@gmail.com>
2017-09-01 19:33:08 +0900

    Update `index` to list actual contents

--- cms/views.py
+++ cms/views.py
@@ -1,7 +1,14 @@
 from django.shortcuts import render
 from django.contrib.auth.decorators import login_required
 
+from .models import Content
+
 # Create your views here.
 @login_required
 def index(request):
-    return render(request, 'cms/index.html')
+    recent_contents = Content.objects.all()[:9]
+    context = {
+        'recent_contents': recent_contents,
+        'no_contents': not bool(recent_contents)
+    }
+    return render(request, 'cms/index.html', context)
