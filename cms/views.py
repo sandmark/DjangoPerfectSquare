1afc0834aa2bc5872eec5f9cf398e985dda1339e
sandmark <sandmark.m@gmail.com>
2017-09-02 22:32:12 +0900

    Fix to use snippet template when listing contents

--- cms/views.py
+++ cms/views.py
@@ -9,10 +9,10 @@ from .models import Content, Tag
 # Create your views here.
 @login_required
 def index(request):
-    recent_contents = Content.objects.all()[:9]
+    contents = Content.objects.all()
     context = {
-        'recent_contents': recent_contents,
-        'no_contents': not bool(recent_contents)
+        'contents': contents,
+        'no_contents': not bool(contents)
     }
     return render(request, 'cms/index.html', context)
 
