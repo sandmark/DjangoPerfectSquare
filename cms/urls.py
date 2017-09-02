ccdc90c8433069785ea38b83fb497597e659b8ba
sandmark <sandmark.m@gmail.com>
2017-09-03 06:02:42 +0900

    Add feature `bulk tagging`

--- cms/urls.py
+++ cms/urls.py
@@ -6,4 +6,7 @@ urlpatterns = [
     url(r'^watch/(?P<content_id>\d+)/$', views.watch, name='watch'),
     url(r'^tags/(?P<tag_id>\d+)/contents/$', views.tagged_contents, name='tagged_contents'),
     url(r'^private-square/register/$', views.register_from_file, name='register_file'),
+    url(r'^tagging/form/$', views.tagging_form, name='tagging'),
+    url(r'^tagging/search/$', views.tagging_search, name='tag_search'),
+    url(r'^tagging/set/$', views.tagging_set, name='tag_set'),
 ]
