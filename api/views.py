b48aa367d760dcb4bd36ebc189030414d5ac35dc
sandmark <sandmark.m@gmail.com>
2017-09-24 18:51:46 +0900

    Refactor to use generic class-based view

--- api/views.py
+++ api/views.py
@@ -3,35 +3,14 @@ api/views.py
 """
 from cms.models import Content
 from api.serializers import ContentSerializer
-from rest_framework import mixins
 from rest_framework import generics
 
 
-class ContentList(mixins.ListModelMixin,
-                  mixins.CreateModelMixin,
-                  generics.GenericAPIView):
+class ContentList(generics.ListCreateAPIView):
     queryset = Content.objects.all()
     serializer_class = ContentSerializer
 
-    def get(self, request, *args, **kwargs):
-        return self.list(request, *args, **kwargs)
 
-    def post(self, request, *args, **kwargs):
-        return self.create(request, *args, **kwargs)
-
-
-class ContentDetail(mixins.RetrieveModelMixin,
-                    mixins.UpdateModelMixin,
-                    mixins.DestroyModelMixin,
-                    generics.GenericAPIView):
+class ContentDetail(generics.RetrieveUpdateDestroyAPIView):
     queryset = Content.objects.all()
     serializer_class = ContentSerializer
-
-    def get(self, request, *args, **kwargs):
-        return self.retrieve(request, *args, **kwargs)
-
-    def put(self, request, *args, **kwargs):
-        return self.update(request, *args, **kwargs)
-
-    def delete(self, request, *args, **kwargs):
-        return self.destroy(request, *args, **kwargs)
