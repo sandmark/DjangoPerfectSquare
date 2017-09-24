622c5c85f38dfab08d4f526a3ae5f49fcb312874
sandmark <sandmark.m@gmail.com>
2017-09-24 18:50:10 +0900

    Refactor to use MixIn

--- api/views.py
+++ api/views.py
@@ -1,57 +1,37 @@
 """
 api/views.py
 """
-from django.http import Http404
-from rest_framework import status
-from rest_framework.response import Response
-from rest_framework.views import APIView
 from cms.models import Content
 from api.serializers import ContentSerializer
+from rest_framework import mixins
+from rest_framework import generics
 
 
-class ContentList(APIView):
-    """
-    List all contents, or create a new content.
-    """
+class ContentList(mixins.ListModelMixin,
+                  mixins.CreateModelMixin,
+                  generics.GenericAPIView):
+    queryset = Content.objects.all()
+    serializer_class = ContentSerializer
 
-    def get(self, request, format=None):
-        contents = Content.objects.all()
-        serializer = ContentSerializer(contents, many=True)
-        return Response(serializer.data)
+    def get(self, request, *args, **kwargs):
+        return self.list(request, *args, **kwargs)
 
-    def post(self, request, format=None):
-        serializer = ContentSerializer(data=request.data)
-        if serializer.is_valid():
-            serializer.save()
-            return Response(serializer.data, status=status.HTTP_201_CREATED)
-        return Response(serializer.errors, status=status.HTTP_400_BAD_REQUEST)
+    def post(self, request, *args, **kwargs):
+        return self.create(request, *args, **kwargs)
 
 
-class ContentDetail(APIView):
-    """
-    Retrieve, update or delete a content.
-    """
+class ContentDetail(mixins.RetrieveModelMixin,
+                    mixins.UpdateModelMixin,
+                    mixins.DestroyModelMixin,
+                    generics.GenericAPIView):
+    queryset = Content.objects.all()
+    serializer_class = ContentSerializer
 
-    def get_object(self, pk):
-        try:
-            return Content.objects.get(pk=pk)
-        except Content.DoesNotExist:
-            raise Http404
+    def get(self, request, *args, **kwargs):
+        return self.retrieve(request, *args, **kwargs)
 
-    def get(self, request, pk, format=None):
-        content = self.get_object(pk)
-        serializer = ContentSerializer(content)
-        return Response(serializer.data)
+    def put(self, request, *args, **kwargs):
+        return self.update(request, *args, **kwargs)
 
-    def put(self, request, pk, format=None):
-        content = self.get_object(pk)
-        serializer = ContentSerializer(content, data=request.data)
-        if serializer.is_valid():
-            serializer.save()
-            return Response(serializer.data)
-        return Response(serializer.errors, status=status.HTTP_400_BAD_REQUEST)
-
-    def delete(self, request, pk, format=None):
-        content = self.get_object(pk)
-        content.delete()
-        return Response(status=status.HTTP_204_NO_CONTENT)
+    def delete(self, request, *args, **kwargs):
+        return self.destroy(request, *args, **kwargs)
