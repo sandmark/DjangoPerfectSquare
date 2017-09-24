3b2568217d670ebffc2edd35502e003f4cc3eea5
sandmark <sandmark.m@gmail.com>
2017-09-24 19:08:54 +0900

    Add required permissions: `IsAuthenticated`

--- api/views.py
+++ api/views.py
@@ -3,24 +3,28 @@ api/views.py
 """
 from cms.models import Content, Tag
 from api.serializers import ContentSerializer, TagSerializer
-from rest_framework import generics
+from rest_framework import generics, permissions
 
 
 class ContentList(generics.ListCreateAPIView):
+    permission_classes = (permissions.IsAuthenticated,)
     queryset = Content.objects.all()
     serializer_class = ContentSerializer
 
 
 class ContentDetail(generics.RetrieveUpdateDestroyAPIView):
+    permission_classes = (permissions.IsAuthenticated,)
     queryset = Content.objects.all()
     serializer_class = ContentSerializer
 
 
 class TagList(generics.ListCreateAPIView):
+    permission_classes = (permissions.IsAuthenticated,)
     queryset = Tag.objects.all()
     serializer_class = TagSerializer
 
 
 class TagDetail(generics.RetrieveUpdateDestroyAPIView):
+    permission_classes = (permissions.IsAuthenticated,)
     queryset = Tag.objects.all()
     serializer_class = TagSerializer
