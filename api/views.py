cb69376c82a3702e7e17f7231fc44492d69936cb
sandmark <sandmark.m@gmail.com>
2017-09-24 19:10:40 +0900

    Fix required permissions to `IsAdminUser`

--- api/views.py
+++ api/views.py
@@ -7,24 +7,24 @@ from rest_framework import generics, permissions
 
 
 class ContentList(generics.ListCreateAPIView):
-    permission_classes = (permissions.IsAuthenticated,)
+    permission_classes = (permissions.IsAdminUser,)
     queryset = Content.objects.all()
     serializer_class = ContentSerializer
 
 
 class ContentDetail(generics.RetrieveUpdateDestroyAPIView):
-    permission_classes = (permissions.IsAuthenticated,)
+    permission_classes = (permissions.IsAdminUser,)
     queryset = Content.objects.all()
     serializer_class = ContentSerializer
 
 
 class TagList(generics.ListCreateAPIView):
-    permission_classes = (permissions.IsAuthenticated,)
+    permission_classes = (permissions.IsAdminUser,)
     queryset = Tag.objects.all()
     serializer_class = TagSerializer
 
 
 class TagDetail(generics.RetrieveUpdateDestroyAPIView):
-    permission_classes = (permissions.IsAuthenticated,)
+    permission_classes = (permissions.IsAdminUser,)
     queryset = Tag.objects.all()
     serializer_class = TagSerializer
