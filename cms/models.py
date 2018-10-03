313f3ba367de4a46895f4436d6a42e0fd6e4fdc2
sandmark <sandmark.m@gmail.com>
2018-10-03 23:50:16 +0900

    Fix when Conten was deleted, the S3 file would be deleted too.

--- cms/models.py
+++ cms/models.py
@@ -46,3 +46,37 @@ class Check(models.Model):
 # def content_delete(sender, instance, **kwargs):
 #     if instance.filepath:
 #         instance.filepath.delete(False)
+
+# So these code below delete S3 file intended by Content.filepath
+# when Content was deleted, however,
+# I still want to solve this problem smarter than this.
+
+from django.db.models.signals import post_delete
+from django.dispatch.dispatcher import receiver
+import urllib
+from boto3.session import Session
+from botocore.exceptions import ClientError
+from django.conf import settings
+
+def is_s3_exists(s3, bucket_name, key):
+    try:
+        s3.Object(bucket_name, key).load()
+    except ClientError as e:
+        if e.response['Error']['Code'] == '404':
+            return False
+        raise e
+    else:
+        return True
+
+@receiver(post_delete, sender=Content)
+def content_delete_file_from_s3(sender, instance, **kwargs):
+    # Parse URI to S3Key
+    parts = urllib.parse.urlparse(instance.filepath)[2].split('/')[2:]
+    key = '/'.join(parts)
+    if key:
+        session = Session(aws_access_key_id=settings.AWS_ACCESS_KEY_ID,
+                          aws_secret_access_key=settings.AWS_SECRET_ACCESS_KEY,
+                          region_name=settings.S3DIRECT_REGION)
+        s3 = session.resource('s3')
+        if is_s3_exists(s3, settings.AWS_STORAGE_BUCKET_NAME, key):
+            s3.Object(settings.AWS_STORAGE_BUCKET_NAME, key).delete()
--- cms/tests/tests_model.py
+++ cms/tests/tests_model.py
@@ -3,6 +3,27 @@ from django.test import TestCase
 from ..models import Content, Check
 from .helpers import create_content, create_user
 
+import os
+from django.conf import settings
+from boto3.session import Session
+from botocore.exceptions import ClientError
+
+test_file = 'cms/tests/test.mp4'
+REGION = settings.S3DIRECT_REGION
+AWS_ACCESS_KEY = settings.AWS_ACCESS_KEY_ID
+AWS_SECRET_ACCESS_KEY = settings.AWS_SECRET_ACCESS_KEY
+BUCKET_NAME = settings.AWS_STORAGE_BUCKET_NAME
+
+def is_s3_exists(s3, bucket, filepath):
+    try:
+        s3.Object(bucket, filepath).load()
+    except ClientError as e:
+        if e.response['Error']['Code'] == '404':
+            return False
+        raise e
+    else:
+        return True
+
 class ContentTest(TestCase):
     def test_if_content_check_deletion(self):
         """
@@ -16,3 +37,51 @@ class ContentTest(TestCase):
         self.assertEqual(Check.objects.count(), 1)
         c.delete()
         self.assertEqual(Check.objects.count(), 0)
+
+    def setup_s3(self, s3, bucket, key):
+        """
+        テスト用のファイルをS3にアップロードする。
+        """
+        if is_s3_exists(s3, BUCKET_NAME, key):
+            s3.Object(BUCKET_NAME, key).delete()
+        bucket.upload_file(test_file, key)
+
+    def after_s3(self, s3, key):
+        """
+        テスト用のファイルをS3から削除する。
+        """
+        if is_s3_exists(s3, BUCKET_NAME, key):
+            s3.Object(BUCKET_NAME, key).delete()
+
+    def test_file_delete_when_content_deleted(self):
+        """
+        Contentレコードが削除されると同時にS3上にあるファイルも削除される。
+        """
+        test_file_basename = os.path.basename(test_file)
+        test_key = 'test/' + test_file_basename
+        session = Session(aws_access_key_id=AWS_ACCESS_KEY,
+                          aws_secret_access_key=AWS_SECRET_ACCESS_KEY,
+                          region_name=REGION)
+        s3 = session.resource('s3')
+        bucket = s3.Bucket(BUCKET_NAME)
+
+        self.setup_s3(s3, bucket, test_key)
+
+        # test code
+        dest = 'https://{region}.amazonaws.com/{bucket_name}/{test_key}'.format(
+            region=REGION,
+            bucket_name=BUCKET_NAME,
+            test_key=test_key,
+        )
+
+        import urllib
+        url = urllib.parse.urlparse(dest)[2].split('/')[2:]
+        url = '/'.join(url)
+
+        content = Content(title='delete_test', filepath=dest)
+        content.save()
+        content.delete()
+        existance = is_s3_exists(s3, BUCKET_NAME, test_key)
+        self.assertFalse(existance)
+
+        self.after_s3(s3, test_key)
