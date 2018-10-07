c39ab666c35c0143f1eabe8faa0720904134c9bf
sandmark <sandmark.m@gmail.com>
2018-10-08 02:48:41 +0900

    Update player style

--- cms/static/cms/layout.js
+++ cms/static/cms/layout.js
@@ -1,11 +1,3 @@
 $(document).ready(function() {
-    $(".dropdown-trigger[data-target='user-dropdown-navbar']").dropdown({
-        hover: true,
-        constrainWidth: false,
-    });
-
-    $(".dropdown-trigger[data-target='tags-dropdown-navbar']").dropdown({
-        hover: false,
-        constrainWidth: true,
-    });
+    $(".materialboxed").materialbox();
 });
